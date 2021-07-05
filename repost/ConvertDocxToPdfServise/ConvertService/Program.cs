7480e68acdaa62be68b6798cf6eb9337733bfb03
SavelyK <zakalennij@bk.ru>
2021-07-05 19:25:03 +0300

    save

--- repost/ConvertDocxToPdfServise/ConvertService/Program.cs
+++ repost/ConvertDocxToPdfServise/ConvertService/Program.cs
@@ -44,27 +44,7 @@ namespace ConvertService
             Console.ReadKey();
 
 
-            //test code block
-            //using (var db = new MyDbContext())
-            //{
-            //    var DbModels = db.Set<DbModel>();
-            //    DbModel file = db.DbModels.FirstOrDefault();
-            //    Print(DbModels);
-            //    db.SaveChanges();
-            //}
-        //}
-        //static void Print(IEnumerable<DbModel> DbModels)
-        //{
-
-        //    foreach (var DbModel in DbModels)
-        //    {
-        //        Console.WriteLine($"Id файла: {DbModel.Id}");
-        //        Console.WriteLine($"Путь к файлу: {DbModel.Path}");
-        //        Console.WriteLine($"Имя файла: {DbModel.FileName}");
-        //        Console.WriteLine($"Время загрузки: {DbModel.LoadTime}");
-        //        Console.WriteLine($"Индикация: {DbModel.Indicator}");
-        //        Console.WriteLine($"приоритет: {DbModel.Priority}");
-        //    }
+          
 
         }
     }
--- repost/ConvertDocxToPdfServise/LibraryModels/DbModel.cs
+++ repost/ConvertDocxToPdfServise/LibraryModels/DbModel.cs
@@ -10,14 +10,6 @@ namespace LibraryModels
         public DateTime LoadTime { get; set; }
         public DateTime TaskTime { get; set; }
         public int Status { get; set; }
-        public string NewPath { get; set; }
-        public string NewFileName { get; set; }
         public int Priority { get; set; }
-        public int TaskIndicator1 { get; set; }
-        public int TaskIndicator2 { get; set; }
-        public int TaskIndicator3 { get; set; }
-        public int TaskIndicator4 { get; set; }
-        public int TaskIndicator5 { get; set; }
-
     }
 }

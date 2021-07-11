f857a14054d10f6196b8ab30e85b51bc492b6b1b
SavelyK <zakalennij@bk.ru>
2021-07-11 03:41:49 +0300

    save

---
+++ ProgectConvertServiceAndServiceApi/ServiceApi/ConversionService/Controllers/TaskStatusController.cs
@@ -0,0 +1,46 @@
+﻿using ConversionService;
+using ConversionService.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskStatusController : ControllerBase
+    {
+        private readonly MyDbContext db;
+        public TaskStatusController(MyDbContext db)
+        {
+            this.db = db;
+        }
+        string iscomplited;
+        [HttpGet("id")]
+        public string Get(int id)
+        {
+            int status;
+            using (var db = new MyDbContext())
+            {
+                DbModel file;
+                file = db.DbModels.FirstOrDefault(p => p.Id == id);
+                 status = file.Status; 
+            }
+            if (status == 3)
+            {
+                iscomplited = "status completed";
+                return iscomplited;
+            }
+            else
+            {
+                iscomplited ="status is being processed";
+                return iscomplited;
+            }
+            
+        }
+        
+    }
+}
